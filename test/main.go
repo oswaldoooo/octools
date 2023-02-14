e836de4613ae5b1b3320c81936084691ada3a16d
oswaldoooo <oswaldohome007@gmail.com>
2023-02-14 14:34:56 +0800

    array sort ,the res has some repeat

--- test/main.go
+++ test/main.go
@@ -3,18 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/oswaldoooo/octools/datastore"
+	"github.com/oswaldoooo/octools/toolsbox"
 )
 
 func main() {
-	stack := datastore.NewListStack[string]()
-	stack.Push("im first in")
-	stack.Push("im second in")
-	stack.Push("im third in")
-	stack.PrintStack()
-	fmt.Println(stack.Peek().GetVal())
-	fmt.Println(stack.Pop().GetVal())
-	stack.PrintStack()
-	stack.Push("im last in")
-	stack.PrintStack()
+	testarr := toolsbox.MakeRandArray(10, []int{10})
+	fmt.Println(testarr)
+	testarr = toolsbox.SortArray(testarr)
+	fmt.Println(testarr)
 }
