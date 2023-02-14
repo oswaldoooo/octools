9189ffaab90487e2f01b26faf93b5ab899630a3d
oswaldoooo <oswaldohome007@gmail.com>
2023-02-14 17:08:24 +0800

    fix array bug

--- test/main.go
+++ test/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/oswaldoooo/octools/toolsbox"
 )
 
 func main() {
-	testarr := toolsbox.MakeRandArray(10, []int{10})
-	fmt.Println(testarr)
-	testarr = toolsbox.SortArray(testarr)
-	fmt.Println(testarr)
+	toolsbox.TestSort()
 }
