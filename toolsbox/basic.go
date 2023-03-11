af89d90b6965c628b59d7b6e9d30013df81786ad
oswaldoooo <oswaldohome007@gmail.com>
2023-03-11 15:55:56 +0800

    update loginit

--- toolsbox/basic.go
+++ toolsbox/basic.go
@@ -1,22 +1,9 @@
 package toolsbox
 
 import (
-	"fmt"
-	"log"
 	"os"
 )
 
 var ROOTPATH = os.Getenv("OCTOOLS_HOME")
 
 // var processlog = loginit("process")
-
-func loginit(logname string) *log.Logger {
-	filepath := ROOTPATH + "logs/" + logname + ".log"
-	fe, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	logger := log.New(fe, "["+logname+"]", log.LUTC|log.Lshortfile|log.LstdFlags)
-	return logger
-}
